baa48917d8dbc421b8b29726da1fe8a288c2a724
DrPeachy <p1067838263@gmail.com>
2023-11-05 01:28:40 -0400

    feat: implemented login Avatar changing based on username existence --Charles

--- backend/src/routes/loginRoutes.mjs
+++ backend/src/routes/loginRoutes.mjs
@@ -1,9 +1,9 @@
 import express from 'express';
-import { User } from '../models/User.mjs';
 import passport from 'passport';
+import mongoose from 'mongoose';
 
 const router = express.Router();
-
+const User = mongoose.model('User');
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login-failure', successRedirect: '/login-success' }));
 
 // Define the login success and failure routes as needed
@@ -15,4 +15,16 @@ router.get('/login-failure', (req, res) => {
   res.json({ message: 'Login failed' });
 });
 
+
+router.post('/checkUsername', async (req, res) => {
+  // Check if username exists in database
+  const username = req.body.username;
+  const result = await User.findOne({ username: username });
+  if (result) {
+    res.json({ message: 'Username existed' });
+  } else {
+    res.json({});
+  }
+
+});
 export default router; 
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,37 +1,70 @@
 import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Container, TextField } from "@mui/material";
+import { Container, Grid, TextField } from "@mui/material";
 import { Button } from "@mui/material";
 import { Box } from "@mui/system";
 import { FormControlLabel } from "@mui/material";
 import { Checkbox } from "@mui/material";
+import { Avatar } from "@mui/material";
+import { Typography } from "@mui/material";
 
 import { useState, useEffect } from "react";
 
 
 const Login = () => {
   const navigate = useNavigate();
+  const [isUsernameExisted, setIsUsernameExisted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData);
     axios.post('/login', data)
-    .then(response => {
-      if (response.data.message === 'Login successful') {
-        navigate('/dashboard');
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => {
+        if (response.data.message === 'Login successful') {
+          navigate('/dashboard');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
+
+  const handleUsernameChange = (event) => {
+    axios.post('/checkUsername', { username: event.target.value })
+      .then(response => {
+        if (response.data.message) {
+          setIsUsernameExisted(true);
+        }else{
+          setIsUsernameExisted(false);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
 
   return (
     <Container component="main" maxWidth="xs">
       <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Avatar 
+            sx={{ width: 80, height: 80, bgcolor: 'secondary.main' }}
+            src={isUsernameExisted?"./assets/logo.png":""}   
+            alt="n"
+          >
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+        </Grid>
         <TextField
           margin="normal"
           required
@@ -40,6 +73,7 @@ const Login = () => {
           label="Username"
           name="username"
           autoFocus
+          onChange={handleUsernameChange}
         />
         <TextField
           margin="normal"
