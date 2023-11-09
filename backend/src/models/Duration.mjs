846a2a02308438354be67faf03072371622857c3
DrPeachy <p1067838263@gmail.com>
2023-11-08 20:09:27 -0500

    fix: adjusted schema to embed document inside resume --Charles

--- backend/src/models/Duration.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const durationSchema = new mongoose.Schema({
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true }
-});
-
-const Duration = mongoose.model('Duration', durationSchema);
-export { Duration, durationSchema };
--- backend/src/models/Resume.mjs
+++ backend/src/models/Resume.mjs
@@ -1,17 +1,48 @@
 import mongoose from "mongoose";
-import { SkillSlot } from "./SkillSlot.mjs";
-import { Education } from "./Education.mjs";
-import { Experience } from "./Experience.mjs";
+// import { SkillSlot } from "./SkillSlot.mjs";
+// import { Education } from "./Education.mjs";
+// import { Experience } from "./Experience.mjs";
 
-const resumeSchema = new mongoose.Schema({
+const skillSlotSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    list: { type: [String], required: true },
+  });
+
+const durationSchema = new mongoose.Schema({
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true }
+});
+
+const experienceSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    type: { type: String, required: true },
+    organization: { type: String, required: true },
+    location: { type: String, required: false },
+    duration: { type: durationSchema, required: true },
+    description: { type: [String], required: true }
+});
+
+const educationSchema = new mongoose.Schema({
+    institution: { type: String, required: true },
+    degree: { type: String, required: true },
+    major: { type: String, required: true },
+    minor: { type: String, required: false },
+    gpa: { type: String, required: true },
+    duration: { type: durationSchema, required: true }
+});
+
+const headerSchema = new mongoose.Schema({
     name: { type: String, required: false },
     phone: { type: String, required: false },
     email: { type: [String], required: false },
-    links: { type: [String], required: false },
-    educations: { type: [Education.schema], required: false },
-    experiences: { type: [Experience.schema], required: false },
-    skills: { type: SkillSlot.schema, required: false }
+    links: { type: [String], required: false }
+});
 
+const resumeSchema = new mongoose.Schema({
+    header: { type: headerSchema, required: false },
+    educations: { type: [educationSchema], required: false },
+    experiences: { type: [experienceSchema], required: false },
+    skills: { type: [skillSlotSchema], required: false }
 });
 
 const Resume = mongoose.model('Resume', resumeSchema);
